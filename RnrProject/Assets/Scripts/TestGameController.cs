dbb5c2e21792372134a15d7f02a621caa22b5b8e
Maathuis FW, Floris <f.w.maathuis@pl.hanze.nl>
2024-12-02 13:34:38 +0100

    Add testscene and script for controller test

---
+++ RnrProject/Assets/Scripts/TestGameController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.DualShock;
+using UnityEngine.InputSystem.XInput;
+using UnityEngine.InputSystem;
+
+public class TestGameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var gamepad = Gamepad.current;
+        if (gamepad == null)
+        {
+            print("No gamepad");
+        }
+        if (gamepad is DualShockGamepad)
+        {
+            print("Playstation gamepad");
+        }
+        else if (gamepad is XInputController)
+        {
+            print("Xbox gamepad");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
