ac241943541055f855e2f47b8536c59ecf5e442c
Усевалад Мартысюк | Usevalad Martysiuk <wsmartysuk@gmail.com>
2025-01-15 17:53:29 +0100

    15.01.2025 Buttons settings
    test:
    - sliders for changing buttons position/size added;

---
+++ RnrProject/Assets/Scripts/Settings/CyllinderSizeSetter.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CyllinderSizeSetter : MonoBehaviour
+{
+    private SceneSettings _sceneSettings;
+    private Transform _buttonTransform;
+    private Vector3 _originalScale;
+
+    private void OnEnable()
+    {
+        _buttonTransform = GetComponent<Transform>();
+        _sceneSettings = _buttonTransform.root.GetComponent<SceneSettings>();
+        _sceneSettings.ButtonsSize += ChangeButtonSize;
+        _buttonTransform = GetComponent<Transform>();
+        _originalScale = transform.localScale;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void ChangeButtonSize(float buttonSizeMultiplier)
+    {
+        _buttonTransform.localScale = new Vector3(_originalScale.x * buttonSizeMultiplier, _originalScale.y, _originalScale.z * buttonSizeMultiplier);
+    }
+
+    private void OnDestroy()
+    {
+        _sceneSettings.ButtonsSize -= ChangeButtonSize;
+    }
+}
---
+++ RnrProject/Assets/Scripts/Settings/SceneSettings.cs
@@ -0,0 +1,48 @@
+using System;
+using UnityEngine;
+
+[ExecuteInEditMode] //allows to do thing that should be done in runtime in editmode too
+public class SceneSettings : MonoBehaviour
+{
+    [SerializeField][Range(-10.0f, 10.0f)]
+    private float distanseFromCamera = 0.0f;
+    private float previousDistanseFromCamera = 0.0f;
+
+    [SerializeField][Range(0.5f, 1.5f)]
+    private float buttonSize = 0.0f;
+    private float previousButtonSize = 0.0f;
+
+    //public event Action<float> ButtonsPosition;
+    public event Action<float> ButtonsSize;
+
+    private Transform _allButtons;
+    private Vector3 _originalPosition;
+
+    void Start()
+    {
+        _allButtons = GetComponent<Transform>();
+        _originalPosition = _allButtons.position;
+    }
+
+    void ChangeButtonsPosition()
+    {
+        //if (previousDistanseFromCamera != distanseFromCamera) ButtonsPosition?.Invoke(distanseFromCamera);
+        if (previousDistanseFromCamera != distanseFromCamera)
+        {
+            Vector3 newPosition = new Vector3(_originalPosition.x, _originalPosition.y, _originalPosition.z + distanseFromCamera);
+            _allButtons.position = newPosition;
+            previousDistanseFromCamera = distanseFromCamera;
+        }
+    }
+
+    void ChangeButtonsSize()
+    {
+        if (previousButtonSize!=buttonSize) ButtonsSize?.Invoke(buttonSize);
+    }
+
+    void Update()
+    {
+        ChangeButtonsSize();
+        ChangeButtonsPosition();
+    }
+}
